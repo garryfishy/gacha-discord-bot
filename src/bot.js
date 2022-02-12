77c24cd2b8eb8ed8c8b83d6ab4b20ca22bb133ea
Garry Agassi <garry.agassi@sprout.co.id>
2022-02-13 01:41:46 +0700

    integrate to api

--- src/bot.js
+++ src/bot.js
@@ -33,12 +33,39 @@ let gif = "";
 const getCharGif = async (charName) => {
   let result = await axios({
     method: "GET",
-    url: `https://g.tenor.com/v1/search?q=${charName}%impact&key=${process.env.TENOR_TOKEN}`,
+    url: `https://g.tenor.com/v1/search?q=${charName}%genshin%impact&key=${process.env.TENOR_TOKEN}`,
   });
   if (result) {
-    gif = result.data.results[0].itemurl;
+    let random = Math.floor(Math.random() * 10);
+    gif = result.data.results[random].itemurl;
   }
 };
+
+let characterGet = "";
+let rarity = 0;
+
+async function getCharacters() {
+  let result = await axios({
+    method: "get",
+    url: "https://api.genshin.dev/characters/",
+  });
+  if (result) {
+    let json = result.data;
+    let number = Math.floor(Math.random() * json.length);
+    characterGet = json[number];
+    console.log(characterGet);
+  }
+}
+
+async function getCharacterDetail() {
+  let result = await axios({
+    method: "get",
+    url: `https://api.genshin.dev/characters/${characterGet}`,
+  });
+  if (result) {
+    rarity = result.data.rarity;
+  }
+}
 
 client.on("messageCreate", async (message) => {
   if (message.author.bot) {
@@ -49,23 +76,15 @@ client.on("messageCreate", async (message) => {
       if (command === "test") {
         message.reply("test function oi");
       }
-      if (command === "gacha") {
-        let dice = Math.floor(Math.random() * 90);
-        if (dice === 90) {
-          await message.channel.send(
-            "https://tenor.com/view/genshin-wish-5star-gif-19762608"
-          );
-          await message.channel.send(
-            `${fiveStars[Math.round(Math.random() * fiveStars.length)]}`
-          );
-        } else {
-          const char = fourStars[Math.round(Math.random() * fourStars.length)];
 
-          console.log(char);
+      if (command.toLowerCase() === "gacha") {
+        await getCharacters();
+        await getCharacterDetail();
+        if (rarity === 4) {
           await message.channel.send(
             "https://tenor.com/view/genshin-impact-gacha-gif-20798126"
           );
-          await getCharGif(char);
+          await getCharGif(characterGet);
           function timeout(ms) {
             return new Promise((resolve) => setTimeout(resolve, ms));
           }
@@ -73,12 +92,69 @@ client.on("messageCreate", async (message) => {
             await timeout(4050);
             await message.channel.send(gif);
             await timeout(500);
-            await message.channel.send(`Hi ${message.author}! You got ${char}`);
+            await message.channel.send(
+              `Hi ${message.author}! You got ${characterGet}`
+            );
           }
-          timeout();
+          sleep();
+        } else if (rarity === 5) {
+          await message.channel.send(
+            "https://tenor.com/view/genshin-wish-5star-gif-19762608"
+          );
+          if (characterGet === "aloy") {
+            gif =
+              "https://tenor.com/view/aloy-genshin-aloy-genshin-impact-aloy-pigeons-genshin-genshin-impact-gif-23266417";
+          } else {
+            await getCharGif(characterGet);
+          }
+          function timeout(ms) {
+            return new Promise((resolve) => setTimeout(resolve, ms));
+          }
+          async function sleep(fn, ...args) {
+            await timeout(4050);
+            await message.channel.send(gif);
+            await timeout(500);
+            await message.channel.send(
+              `Hi ${message.author}! You got ${characterGet}`
+            );
+          }
           sleep();
         }
+
+        console.log(characterGet, rarity);
       }
+
+      //   if (command === "gacha") {
+      //     let dice = Math.floor(Math.random() * 90);
+      //     if (dice === 90) {
+      //       await message.channel.send(
+      //         "https://tenor.com/view/genshin-wish-5star-gif-19762608"
+      //       );
+      //       await message.channel.send(
+      //         `${fiveStars[Math.round(Math.random() * fiveStars.length)]}`
+      //       );
+      //     } else {
+      //       const char = fourStars[Math.round(Math.random() * fourStars.length)];
+
+      //       await message.channel.send(
+      //         "https://tenor.com/view/genshin-impact-gacha-gif-20798126"
+      //       );
+      //       await getCharacters();
+      //       await getCharacterDetail();
+      //       await getCharGif(char);
+      //       function timeout(ms) {
+      //         return new Promise((resolve) => setTimeout(resolve, ms));
+      //       }
+      //       async function sleep(fn, ...args) {
+      //         await timeout(4050);
+      //         await message.channel.send(gif);
+      //         await timeout(500);
+      //         await message.channel.send(`Hi ${message.author}! You got ${char}`);
+      //       }
+      //       timeout();
+      //       sleep();
+      //     }
+      //   }
     }
   }
 });
